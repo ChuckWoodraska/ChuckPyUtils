001a2d92c39f0e6f7b2baa432465752ef7d3cca4
Chuck Woodraska <chuck@blacklanternsecurity.com>
2017-04-25 09:43:16 -0400

    Attempting to use setup.py for -e pip. - CW

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='chuck-pyutils',
+    version='0.1',
+    packages=['scripts'],
+    url='',
+    license='',
+    author='cwoodraska',
+    author_email='chuck.woodraska@gmail.com',
+    description='',
+    requires=[]
+)
